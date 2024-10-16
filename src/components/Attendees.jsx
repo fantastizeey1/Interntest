ac93865ce9da3536926d9c9ec7b0ec649db422fa
Your Name <fantastizeey@gmail.com>
2024-10-16 16:53:59 +0100

    checking for mobile veiw

--- src/components/Attendees.jsx
+++ src/components/Attendees.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Attendees = () => {
   return (
-    <section className="max-w-[95%] w-full px-4 md:px-[80px] mx-auto mt-10 lg:mt-[180px] gap-10 flex flex-col justify-between items-center relative">
+    <section className="max-w-[95%] w-full px-4 md:px-[50px] 2xl:px-[80px] mx-auto mt-10 lg:mt-[180px] gap-10 flex flex-col justify-between items-center relative">
       <div className="flex flex-col justify-center items-center mb-[100px]">
         <h2 className="font-bold text-[28px] text-center md:text-[35px] lg:text-[45px] text-black leading-[36px] md:leading-[46px] lg:leading-[56px] mb-[20px]">
           What Do Our Attendees Have To Say?
@@ -13,12 +13,12 @@ const Attendees = () => {
         <img
           src="/Frame 640.png"
           alt="button"
-          className="absolute top-[10%] md:top-0 lg:right-0 cursor-pointer mt-4"
+          className="absolute top-[10%] md:top-[6%] lg:right-0 cursor-pointer mt-4"
         />
       </div>
 
       {/* Reviews Section */}
-      <div className="flex flex-col md:flex-row justify-center items-center mt-6 gap-12 md:gap-[100px]">
+      <div className="flex flex-col md:flex-row justify-center items-center mt-6 gap-12 md:gap-[50px] 2xl:gap-[100px]">
         <img src="/Frame 634.png" alt="reviews" className="w-full md:w-auto" />
         <img src="/Frame 634.png" alt="reviews" className="w-full md:w-auto" />
         <img src="/Frame 634.png" alt="reviews" className="w-full md:w-auto" />
